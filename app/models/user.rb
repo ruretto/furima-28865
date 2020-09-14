cee725256b0325b34b04047595be12396f425148
ruretto <69375207+ruretto@users.noreply.github.com>
2020-09-14 12:17:15 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ApplicationRecord
        validates :nickname, :password, :password_confirmation, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, presence: true 
        validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "can't be blank"}
        validates :password, length: { with:/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, minimum: 6 }
-         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-       validates :email, format: { with: VALID_EMAIL_REGEX, message: "Email can't be blank"} 
        validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "cannot be registered if it is empty"}
        validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "cannot be registered if it is empty"}
        validates :family_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "cannot be registered if it is empty"}
